5a60469e7b3b0b05c908c6624815a52b7982f205
rlevin <r.levin@salt-and-pepper.eu>
2018-11-16 16:44:55 +0100

    small physics playaround

---
+++ FridayAfternoonTest/Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GroundChecker : MonoBehaviour {
+    private bool isTouchingGround;
+
+    private void Start()
+    {
+        this.isTouchingGround = false;
+    }
+
+    private void OnTriggerEnter()
+    {
+        this.isTouchingGround = true;
+    }
+
+    private void OnTriggerExit()
+    {
+        this.isTouchingGround = false;
+    }
+
+    public bool IsTouchingGround()
+    {
+        return this.isTouchingGround;
+    }
+}
